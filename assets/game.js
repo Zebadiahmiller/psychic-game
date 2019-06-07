31a6e7fd454cf568d57b8b8e3366a4b0ce16e239
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-06-06 22:29:18 -0700

    Game and css are working correctly

--- assets/game.js
+++ assets/game.js
@@ -5,54 +5,52 @@ let wins = 0;
 let losses = 0;
 let guessesLeft = 9;
 let usersGuess = [];
-let computerGuess =[];
+let computerGuess = [];
 
 
 
-window.onload =function startgame(){
-    const letterChosen = letters[Math.floor(Math.random() * letters.length)];
-    computerGuess.push(letterChosen);
-    console.log(computerGuess[0]);
-}
+let letterChosen = letters[Math.floor(Math.random() * letters.length)];
+	
+computerGuess.push(letterChosen);
+console.log("THIS IS THE ANSWER", computerGuess[0]);
 
 
 
-document.onkeyup =function(event){
-    const keyPess= event.key;
-    usersGuess.push(keyPess);
-    console.log(usersGuess);
+document.onkeyup = function (event) {
+	const keyPess = event.key;
+	usersGuess.push(keyPess);
+	console.log("THIS IS MY GUESS", usersGuess);
 
 
 
-if ((usersGuess[0] === computerGuess[0]) && (guessesLeft > 0)) {
-	wins++;
-	guessesLeft = 9;
-	usersGuess.length = 0;
-	computerGuess.length = 0;
-	const letterChosen = letters[Math.floor(Math.random() * letters.length)];
-	computerGuess.push(letterChosen);
-	console.log(computerGuess[0]);
-}
+	if (keyPess === computerGuess[0] && guessesLeft > 0) {
+		wins++;
+		guessesLeft = 9;
+		usersGuess.length = 0;
+		computerGuess = [];
+		letterChosen = letters[Math.floor(Math.random() * letters.length)];
+		computerGuess.push(letterChosen);
+		console.log(computerGuess[0]);
+	}
 
-else if ((usersGuess !== computerGuess[0]) && (guessesLeft > 0)) {
-	guessesLeft = guessesLeft-1;
-}
+	else if ((usersGuess !== computerGuess[0]) && (guessesLeft > 0)) {
+		guessesLeft = guessesLeft - 1;
+	}
 
-else {
-	losses++;
-	guessesLeft = 9;
-	usersGuess.length = 0;
-	computerGuess.length = 0;
-	let computerGuess = letters[Math.floor(Math.random() * letters.length)];
-	computerGuess.push(letterChosen);
-	console.log(computerGuess[0]);
-}
-let html = "<p>Guess what letter I'm thinking of!</p>" +
-          "<p>Wins: " + wins + "</p>" +
-          "<p>Losses: " + losses + "</p>" +
-          "<p>Guesses left: " + guessesLeft + "</p>" +
-          "<p>Your guesses so far: " + usersGuess + "</p>";
- 
-document.querySelector("#game").innerHTML = html;
+	else {
+		losses++;
+		guessesLeft = 9;
+		usersGuess.length = 0;
+		computerGuess = [letters[Math.floor(Math.random() * letters.length)]];
+		computerGuess.push(letterChosen);
+		console.log(computerGuess[0]);
+	}
+	let html = "<p>Guess what letter I'm thinking of!</p>" +
+		"<p>Wins: " + wins + "</p>" +
+		"<p>Losses: " + losses + "</p>" +
+		"<p>Guesses left: " + guessesLeft + "</p>" +
+		"<p>Your guesses so far: " + usersGuess + "</p>";
+
+	document.querySelector("#game").innerHTML = html;
 
 }
